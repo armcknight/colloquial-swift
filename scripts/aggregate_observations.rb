8dc6b22a1890f0b2727dee0695eaf09bbd2d6985
Andrew McKnight <amcknight2718@gmail.com>
2018-07-20 14:34:52 -0600

    refer to ‘cocoa’ instead of ‘cocoa touch’

--- scripts/aggregate_observations.rb
+++ scripts/aggregate_observations.rb
@@ -49,14 +49,14 @@ repo_sets.each_with_index do |repo_set, i|
   cocoa_prefixes = [ 'AB', 'AC', 'AD', 'AL', 'AU', 'AV', 'CA', 'CB', 'CF', 'CG', 'CI', 'CL', 'CM', 'CV', 'EA', 'EK', 'GC', 'GLK', 'JS', 'MA', 'MC', 'MF', 'MIDI', 'MK', 'MP', 'NK', 'NS', 'PK', 'QL', 'SC', 'Sec', 'SK', 'SL', 'SS', 'TW', 'UI', 'UT' ]
   
   exclusion_expr = cocoa_prefixes.map{|x| '-e "^\"' + x + '.*"'}.join(' ')
-  non_cocoa_touch_extensions = massage_results `cat #{repo_set.join(' ')} | jq '.declarations.extension.parsed[].identifier' | grep -v #{exclusion_expr} | sort | uniq -c | sort`
+  non_cocoa_extensions = massage_results `cat #{repo_set.join(' ')} | jq '.declarations.extension.parsed[].identifier' | grep -v #{exclusion_expr} | sort | uniq -c | sort`
 
   # count unique extensions (only 1 declaration found)
   unique_extensions = massage_results(`cat #{repo_set.join(' ')} | jq '.declarations.extension.parsed[].identifier' | sort | uniq -c | sort | grep "   1"`).keys
   
   aggregations_hash[i] = {
     'extensions' => extensions,
-    'non_cocoa_touch_extensions' => non_cocoa_touch_extensions,
+    'non_cocoa_extensions' => non_cocoa_extensions,
     'unique_extensions' => unique_extensions,
   }
 end
@@ -68,12 +68,12 @@ end
 # { 
 #   1 => {
 #     'extensions' => { "Some API" => 614, "Other API" => 5 },
-#     'non_cocoa_touch_extensions' => { "Some API" => 614, "Other API" => 5 },
+#     'non_cocoa_extensions' => { "Some API" => 614, "Other API" => 5 },
 #     'unique_extensions' => { "unique api 1", "unique api 2" },  
 #   },
 #   2 => {
 #     'extensions' => { "Other API" => 46, "Yet another API" => 3 },
-#     'non_cocoa_touch_extensions' => { "etc" => 2, "et al" => 1 },
+#     'non_cocoa_extensions' => { "etc" => 2, "et al" => 1 },
 #     'unique_extensions' => { "unique api 2", "unique api 3" },  
 #   },
 # }
@@ -82,17 +82,17 @@ end
 #
 # {
 #   'extensions' => { "Some API" => 614, "Other API" => 51, "Yet another API" => 3 },
-#   'non_cocoa_touch_extensions' => { "Some API" => 614, "Other API" => 5, "etc" => 2, "et al" => 1  },
+#   'non_cocoa_extensions' => { "Some API" => 614, "Other API" => 5, "etc" => 2, "et al" => 1  },
 #   'unique_extensions' => { "unique api 1", "unique api 3" },
 # }
 
 all_extensions = Hash.new
-all_non_cocoa_touch_extensions = Hash.new
+all_non_cocoa_extensions = Hash.new
 all_unique_extensions = Set.new
 
 aggregations_hash.each do |repo_set, aggregations|
   all_extensions.merge!(aggregations['extensions']) {|key, a_val, b_val| a_val.to_i + b_val.to_i }
-  all_non_cocoa_touch_extensions.merge!(aggregations['non_cocoa_touch_extensions']) {|key, a_val, b_val| a_val.to_i + b_val.to_i }
+  all_non_cocoa_extensions.merge!(aggregations['non_cocoa_extensions']) {|key, a_val, b_val| a_val.to_i + b_val.to_i }
   aggregations['unique_extensions'].each {|unique_extension| all_unique_extensions << unique_extension }
 end
 
@@ -105,8 +105,8 @@ File.open("#{aggregations_dir}/extensions.json", 'w') do |file|
   file << JSON.dump(all_extensions)
 end
 
-File.open("#{aggregations_dir}/non_cocoa_touch_extensions.json", 'w') do |file|
-  file << JSON.dump(all_non_cocoa_touch_extensions)
+File.open("#{aggregations_dir}/non_cocoa_extensions.json", 'w') do |file|
+  file << JSON.dump(all_non_cocoa_extensions)
 end
 
 File.open("#{aggregations_dir}/unique_extensions.json", 'w') do |file|
@@ -116,4 +116,4 @@ end
 # write consolidated, simple text versions of the results
 
 `jq '' #{aggregations_dir}/extensions.json | awk -F'":' '{print $2 $1};' | sort -rn | sed s/\\\"//g | sed s/,//g > #{aggregations_dir}/extensions.simple.txt`
-`jq '' #{aggregations_dir}/non_cocoa_touch_extensions.json | awk -F'":' '{print $2 $1};' | sort -rn | sed s/\\\"//g | sed s/,//g > #{aggregations_dir}/non_cocoa_touch_extensions.simple.txt`
+`jq '' #{aggregations_dir}/non_cocoa_extensions.json | awk -F'":' '{print $2 $1};' | sort -rn | sed s/\\\"//g | sed s/,//g > #{aggregations_dir}/non_cocoa_extensions.simple.txt`
